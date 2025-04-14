0e546b50af18efdf656c0f4d77e6c0a8785ac3b1
Chizumi <okdelvin8@gmail.com>
2025-04-14 18:30:00 +0700

    p

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,21 @@
     <x-navigation />
     <div class="bg-[#FFF8E6]">
         <section class="relative bg-[#E5CBA6] overflow-hidden font-[Inika] min-h-145">
-            <div class="absolute top-0 left-0 w-full mt-8" style="height: 600px;">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 600" preserveAspectRatio="none" class="w-full h-auto">
+            <div class="absolute top-20 sm:top-0 left-0 w-full mt-8 h-[500px] sm:h-[600px]">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 600" preserveAspectRatio="none"
+                    class="w-full h-full">
                     <path fill="#F3E8CC" fill-opacity="1"
                         d="M0,300L48,285.5C96,271,192,242,288,233.3C384,224,480,240,576,265.7C672,291,768,326,864,353.2C960,380,1056,392,1152,368.2C1248,344,1344,271,1392,248.2L1440,225L1440,600L1392,600C1344,600,1248,600,1152,600C1056,600,960,600,864,600C768,600,672,600,576,600C480,600,384,600,288,600C192,600,96,600,48,600L0,600Z">
                     </path>
                 </svg>
             </div>
-            <div class="relative z-10 max-w-7xl mx-auto px-6 py-10 flex flex-col md:flex-row items-start md:items-start text-center md:text-left">
+
+            <div
+                class="relative z-10 max-w-7xl mx-auto px-6 py-10 flex flex-col md:flex-row items-start md:items-start text-center md:text-left">
                 <div class="md:w-1/2 text-center md:text-left flex flex-col justify-start items-center md:items-start">
                     <div>
-                        <h1 class="text-4xl md:text-6xl font-bold text-[#3F2812]">Discovering Caterings Made Easier!</h1>
+                        <h1 class="text-4xl md:text-6xl font-bold text-[#3F2812]">Discovering Caterings Made Easier!
+                        </h1>
                         <p class=" md:text-3x1 mt-4 font-bold text-[#824C0F]">
                             Find new and tasty food all around you with ease! With NomNom, you can connect with your
                             favourite dishes; no matter where or when you are.
@@ -31,7 +35,7 @@
                 </div>
             </div>
         </section>
-        
+
         <x-topmenu />
     </div>
     <x-customerReviews />
