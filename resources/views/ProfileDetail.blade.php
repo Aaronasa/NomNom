85ae7f98e0881504cbdd6e5ed56ac8bcfd8d03aa
Chizumi <okdelvin8@gmail.com>
2025-05-05 21:10:51 +0700

    p

--- resources/views/ProfileDetail.blade.php
+++ resources/views/ProfileDetail.blade.php
@@ -103,7 +103,7 @@
                                 <!-- Submit Button -->
                                 <div class="mt-8">
                                     <button type="submit" 
-                                            class="w-full bg-[#E5CBA6] text-white font-medium py-3 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            class="w-full bg-[#E5CBA6] text-white font-medium py-3 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#E5CBA6]">
                                         Update Profile
                                     </button>
                                 </div>
@@ -111,7 +111,7 @@
 
                             <!-- Back Link -->
                             <div class="mt-6 text-center">
-                                <a href="/home" class="inline-flex items-center text-[#E5CBA6] hover:text-indigo-500 font-medium">
+                                <a href="/home" class="inline-flex items-center text-[#E5CBA6] hover:text-[#d7bb93] font-medium">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
                                     </svg>
