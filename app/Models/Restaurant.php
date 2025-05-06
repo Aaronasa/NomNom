f990c421761c953fc8c8fde79c249d26458e426d
Aaron Asa <aaron.soelistiono22@gmail.com>
2025-05-06 20:23:07 +0700

    Fixing gacor

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -14,15 +14,37 @@ class Restaurant extends Model
         'restaurantName',
         'restaurantAddress',
         'restaurantPhone',
+        'user_id',
     ];
 
     public function restaurantToFood(): HasMany {
         return $this->hasMany(Food::class, 'restaurant_id');
     }
+    public function owner(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     protected static function boot()
     {
         parent::boot();
+
+        static::creating(function ($restaurant) {
+            if (isset($restaurant->user_id)) {
+                $user = User::find($restaurant->user_id);
+                if (!$user || $user->role_id !== 3) {
+                    throw new \Exception('Restaurant harus dimiliki oleh user dengan role restaurant owner (role_id = 3)');
+                }
+            }
+        });
+
+        static::updating(function ($restaurant) {
+            if ($restaurant->isDirty('user_id') && $restaurant->user_id) {
+                $user = User::find($restaurant->user_id);
+                if (!$user || $user->role_id !== 3) {
+                    throw new \Exception('Restaurant harus dimiliki oleh user dengan role restaurant owner (role_id = 3)');
+                }
+            }
+        });
 
         static::deleting(function ($restaurant) {
             // Delete all related Foods
