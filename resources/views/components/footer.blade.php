07b9cf945437c0a9b462c3d0b9e891547572f4ca
Chizumi <okdelvin8@gmail.com>
2025-05-27 00:20:44 +0700

    p

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -59,24 +59,29 @@
         </div>
         <div class="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
             <p class="text-sm text-gray-600">
-                © Copyright 2020 Lorem Inc. All rights reserved.
+                © 2025 NomNom Catering Marketplace. All rights reserved.
             </p>
             <ul class="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
                 <li>
-                    <a href="/"
-                        class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">F.A.Q</a>
+                    <a href="/faq"
+                        class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                        F.A.Q
+                    </a>
                 </li>
                 <li>
-                    <a href="/"
-                        class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">Privacy
-                        Policy</a>
+                    <a href="/privacy-policy"
+                        class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                        Kebijakan Privasi
+                    </a>
                 </li>
                 <li>
-                    <a href="/"
-                        class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">Terms
-                        &amp; Conditions</a>
+                    <a href="/terms-conditions"
+                        class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+                        Syarat & Ketentuan
+                    </a>
                 </li>
             </ul>
         </div>
+
     </div>
 </footer>
